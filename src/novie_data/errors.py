b59e00f25965af295c4a206f2c6c37d99310f39c
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-10-22 23:29:33 +1100

    `errors`: Add verifier function for non negative values
    Raises the custom error type if given value is negative.

--- src/novie_data/errors.py
+++ src/novie_data/errors.py
@@ -1,5 +1,7 @@
 """Various novie data exceptions."""
 
+import optype as op
+
 
 class NegativeValueError(ValueError):
     """Validation error for when the given value is negative."""
@@ -7,3 +9,18 @@ class NegativeValueError(ValueError):
 
 class InconsistentArrayLengthError(ValueError):
     """Validation error for when the given arrays differ in length."""
+
+
+def verify_value_is_nonnegative(value: op.CanLt[int, bool], *, msg: str) -> None:
+    """Verify that the given value is non-negative.
+
+    Parameters
+    ----------
+    value : number
+        The value to check.
+    msg : str
+        The message to display on failure.
+
+    """
+    if value < 0:
+        raise NegativeValueError(msg)

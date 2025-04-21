40f9ba33d0a747288525a72fd9628d06a77402c1
Pavadol Yamsiri <pavyamsiri@gmail.com>
2025-04-21 20:01:49 +1000

    perturber_data: Use generated PerturberData

--- src/novie_data/perturber_data.py
+++ src/novie_data/perturber_data.py
@@ -1,164 +1,241 @@
-"""The animation data class for side on view projections."""
-
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING, ClassVar, Self, TypeAlias
+from typing import TYPE_CHECKING, ClassVar, Protocol, Self, TypeAlias
 
 import numpy as np
 from h5py import File as Hdf5File
 from packaging.version import Version
+from typing_extensions import override
 
-from novie_data._type_utils import Array1D, Array2D, verify_array_is_1d, verify_array_is_2d
-from novie_data.errors import verify_arrays_are_consistent, verify_arrays_have_correct_length
-
-from .serde.accessors import get_str_attr_from_hdf5, read_dataset_from_hdf5_with_dtype
-from .serde.verification import verify_file_type_from_hdf5, verify_file_version_from_hdf5
+from novie_data_gen import (
+    check_axis_length,
+    get_dataset_from_hdf5,
+    get_file_version,
+    get_float_attr_from_hdf5,
+    get_int_attr_from_hdf5,
+    get_str_attr_from_hdf5,
+    read_dataset_from_hdf5_with_dtype,
+    verify_array_is_1d,
+    verify_array_is_2d,
+    verify_array_is_3d,
+    verify_array_is_4d,
+)
 
 if TYPE_CHECKING:
+    from collections.abc import Mapping
     from pathlib import Path
 
+    from novie_data_gen import Array1D, Array2D, Array3D, Array4D
 
-_Array1D_f32: TypeAlias = Array1D[np.float32]
-_Array2D_f32: TypeAlias = Array2D[np.float32]
+    _Array1D_f32: TypeAlias = Array1D[np.float32]
+    _Array2D_f32: TypeAlias = Array2D[np.float32]
+    _Array1D_b8: TypeAlias = Array1D[np.bool_]
+    _Array1D_f64: TypeAlias = Array1D[np.float64]
+    _Array2D_f64: TypeAlias = Array2D[np.float64]
+
+LATEST_VERSION_V2 = Version("2.0.0")
+LATEST_VERSION_V3 = Version("3.0.0")
+
 
 log: logging.Logger = logging.getLogger(__name__)
 
 
+class _PerturberDataLoader(Protocol):
+    def __call__(self, file: Hdf5File) -> PerturberData: ...
+
+
 class PerturberData:
-    """The position, velocity and mass of a single perturber.
-
-    Attributes
-    ----------
-    name : str
-        The name of the dataset.
-    position : NDArray[float]
-        The 3D position at every frame in kpc.
-    velocity : NDArray[float]
-        The 3D velocity at every frame in km/s.
-    mass : NDArray[float]
-        The mass at every frame in Msol.
-
-    """
-
     DATA_FILE_TYPE: ClassVar[str] = "Perturber"
-    VERSION: ClassVar[Version] = Version("2.0.0")
+    VERSION: ClassVar[Version] = LATEST_VERSION_V3
 
-    def __init__(self, *, name: str, position: _Array2D_f32, velocity: _Array2D_f32, mass: _Array1D_f32) -> None:
-        """Perform post-initialisation verification.
 
-        Parameters
-        ----------
-        name : str
-            The name of the dataset.
-        position : Array2D[f32]
-            The 3D position at every frame in kpc.
-        velocity : Array2D[f32]
-            The 3D velocity at every frame in km/s.
-        mass : Array1D[f32]
-            The mass at every frame in Msol.
-
-        """
+    def __init__(
+        self,
+        *,
+        mass: _Array1D_f64,
+        position: _Array2D_f64,
+        velocity: _Array2D_f64,
+        completeness: _Array1D_b8 | bool = True,
+        name: str = "UNKNOWN",
+    ) -> None:
+        _ = check_axis_length(
+            ((0, position.shape), (0, velocity.shape)), expected=3
+        )
+        n = check_axis_length(
+            ((0, mass.shape), (1, position.shape), (1, velocity.shape))
+        )
+        match completeness:
+            case True:
+                completeness = np.ones((n,), dtype=np.bool_)
+            case False:
+                completeness = np.zeros((n,), dtype=np.bool_)
+            case _:
+                pass
+        assert completeness is not bool
+        self.num_frames: int = n
         self.name: str = name
-        self.position: _Array2D_f32 = position
-        self.velocity: _Array2D_f32 = velocity
-        self.mass: _Array1D_f32 = mass
+        self.completeness: _Array1D_b8 = completeness
+        self.mass: _Array1D_f64 = mass
+        self.position: _Array2D_f64 = position
+        self.velocity: _Array2D_f64 = velocity
 
-        # Validate projection
-        verify_arrays_have_correct_length(
-            [(self.position, 0), (self.velocity, 0)], 3, msg="Expected the position/velocity vectors to have 3 rows."
-        )
-        verify_arrays_are_consistent(
-            [(self.position, 1), (self.velocity, 1), (self.mass, 0)],
-            msg="Expected position, velocity and mass to have the same number of frames!",
-        )
-
-    def __eq__(self, other: object, /) -> bool:
-        """Compare for equality.
-
-        Parameters
-        ----------
-        other : object
-            The object to compare to.
-
-        Returns
-        -------
-        bool
-            `True` if the other object is equal to this object, `False` otherwise.
-
-        Notes
-        -----
-        Equality means all fields are equal.
-
-        """
+    @override
+    def __eq__(self, other: object) -> bool:
         if not isinstance(other, type(self)):
             return False
         equality = True
+        equality &= self.num_frames == other.num_frames
         equality &= self.name == other.name
-        equality &= np.all(self.position == other.position)
-        equality &= np.all(self.velocity == other.velocity)
-        equality &= np.all(self.mass == other.mass)
+        equality &= np.array_equal(self.completeness, other.completeness)
+        equality &= np.array_equal(self.mass, other.mass)
+        equality &= np.array_equal(self.position, other.position)
+        equality &= np.array_equal(self.velocity, other.velocity)
         return bool(equality)
 
     @classmethod
-    def load(cls, path: Path) -> Self:
-        """Deserialize data from disk.
-
-        Parameters
-        ----------
-        path : Path
-            The path to the data.
-
-        Returns
-        -------
-        PerturberData
-            The deserialized data.
-
-        """
-        with Hdf5File(path, "r") as file:
-            verify_file_type_from_hdf5(file, cls.DATA_FILE_TYPE)
-            verify_file_version_from_hdf5(file, cls.VERSION)
-
-            name: str = get_str_attr_from_hdf5(file, "name")
-
-            # Projections
-            position = verify_array_is_2d(read_dataset_from_hdf5_with_dtype(file, "position", dtype=np.float32))
-            velocity = verify_array_is_2d(read_dataset_from_hdf5_with_dtype(file, "velocity", dtype=np.float32))
-            mass = verify_array_is_1d(read_dataset_from_hdf5_with_dtype(file, "mass", dtype=np.float32))
-
-        log.info("Successfully loaded [cyan]%s[/cyan] from [magenta]%s[/magenta]", cls.__name__, path.absolute())
+    def empty(cls, *, n: int) -> Self:
+        completeness: _Array1D_b8 = np.zeros((n,), dtype=np.bool_)
+        mass: _Array1D_f64 = np.zeros((n,), dtype=np.float64)
+        position: _Array2D_f64 = np.zeros((3, n), dtype=np.float64)
+        velocity: _Array2D_f64 = np.zeros((3, n), dtype=np.float64)
         return cls(
+            completeness=completeness,
+            mass=mass,
             position=position,
             velocity=velocity,
-            mass=mass,
-            name=name,
         )
 
+    @staticmethod
+    def load(path: Path) -> PerturberData:
+        path = path.expanduser()
+        cls = PerturberData
+        with Hdf5File(path, "r") as file:
+            assert str(file.attrs["type"]) == cls.DATA_FILE_TYPE
+
+            file_version = get_file_version(file)
+            assert file_version.major in _LOADERS
+            data = _LOADERS[file_version.major](file)
+
+        log.info("Successfully dumped [cyan]%s[/cyan] to [magenta]%s[/magenta]", cls.__name__, path)
+        return data
+
+    @classmethod
+    def migrate(cls, path: Path) -> None:
+        path = path.expanduser()
+        with Hdf5File(path, "r") as file:
+            assert str(file.attrs["type"]) == cls.DATA_FILE_TYPE
+
+            file_version = get_file_version(file)
+            if file_version == LATEST_VERSION_V3:
+                return
+            file_version = get_file_version(file)
+            assert file_version.major in _LOADERS
+            data = _LOADERS[file_version.major](file)
+
+        data.dump(path)
+
     def dump(self, path: Path) -> None:
-        """Serialize data to disk.
-
-        Parameters
-        ----------
-        path : Path
-            The path to the data.
-
-        """
+        path = path.expanduser()
         cls = type(self)
         with Hdf5File(path, "w") as file:
-            # General
             file.attrs["type"] = cls.DATA_FILE_TYPE
             file.attrs["version"] = str(cls.VERSION)
             file.attrs["name"] = self.name
-
-            file.create_dataset("position", data=self.position)
-            file.create_dataset("velocity", data=self.velocity)
-            file.create_dataset("mass", data=self.mass)
-
+            _ = file.create_dataset("completeness", data=self.completeness)
+            _ = file.create_dataset("mass", data=self.mass)
+            _ = file.create_dataset("position", data=self.position)
+            _ = file.create_dataset("velocity", data=self.velocity)
         log.info("Successfully dumped [cyan]%s[/cyan] to [magenta]%s[/magenta]", cls.__name__, path.absolute())
 
-    # Convenience functions
+    @classmethod
+    def save_init(cls, path: Path, *, name: str) -> None:
+        path = path.expanduser()
+        if not path.is_file():
+            msg = f"Can't save initial data to {cls.__name__} as it doesn't exist!"
+            raise ValueError(msg)
 
-    @property
-    def num_frames(self) -> int:
-        """int: The number of frames."""
-        return self.position.shape[1]
+        cls.migrate(path)
+        with Hdf5File(path, "a") as file:
+            file.attrs["name"] = name
+        log.info("Successfully saved attributes of [cyan]%s[/cyan] to [magenta]%s[/magenta]", cls.__name__, path)
+
+    @classmethod
+    def save_frame(
+        cls,
+        path: Path,
+        frame: int,
+        *,
+        mass: float,
+        position: _Array1D_f64,
+        velocity: _Array1D_f64,
+    ) -> None:
+        path = path.expanduser()
+        if not path.is_file():
+            msg = f"Can't save initial data to {cls.__name__} as it doesn't exist!"
+            raise ValueError(msg)
+
+        _ = check_axis_length(
+            ((0, position.shape), (0, velocity.shape)), expected=3
+        )
+        cls.migrate(path)
+        with Hdf5File(path, "a") as file:
+            get_dataset_from_hdf5(file, "completeness").write_direct(
+                np.asarray(True, dtype=np.bool_).reshape(1), np.s_[0], np.s_[frame]
+            )
+            get_dataset_from_hdf5(file, "mass").write_direct(
+                np.asarray(mass, dtype=np.float64).reshape(1), np.s_[0], np.s_[frame]
+            )
+            get_dataset_from_hdf5(file, "position").write_direct(
+                np.asarray(position, dtype=np.float64).reshape((3,)), np.s_[:], np.s_[:, frame]
+            )
+            get_dataset_from_hdf5(file, "velocity").write_direct(
+                np.asarray(velocity, dtype=np.float64).reshape((3,)), np.s_[:], np.s_[:, frame]
+            )
+        log.info("Successfully saved frame {frame} of [cyan]%s[/cyan] to [magenta]%s[/magenta]", cls.__name__, path)
+
+
+def load_v2(file: Hdf5File) -> PerturberData:
+    cls = PerturberData
+    name = get_str_attr_from_hdf5(file, "name")
+    mass = verify_array_is_1d(read_dataset_from_hdf5_with_dtype(file, "mass", dtype=np.float32))
+    position = verify_array_is_2d(read_dataset_from_hdf5_with_dtype(file, "position", dtype=np.float32))
+    velocity = verify_array_is_2d(read_dataset_from_hdf5_with_dtype(file, "velocity", dtype=np.float32))
+    n = check_axis_length(
+        ((0, mass.shape), (1, position.shape), (1, velocity.shape),)
+    )
+
+    return cls(
+        name=name,
+        mass=mass.astype(np.float64),
+        position=position.astype(np.float64),
+        velocity=velocity.astype(np.float64),
+    )
+
+
+def load_v3(file: Hdf5File) -> PerturberData:
+    cls = PerturberData
+    name = get_str_attr_from_hdf5(file, "name")
+    completeness = verify_array_is_1d(read_dataset_from_hdf5_with_dtype(file, "completeness", dtype=np.bool_))
+    mass = verify_array_is_1d(read_dataset_from_hdf5_with_dtype(file, "mass", dtype=np.float64))
+    position = verify_array_is_2d(read_dataset_from_hdf5_with_dtype(file, "position", dtype=np.float64))
+    velocity = verify_array_is_2d(read_dataset_from_hdf5_with_dtype(file, "velocity", dtype=np.float64))
+    n = check_axis_length(
+        ((0, mass.shape), (1, position.shape), (1, velocity.shape),)
+    )
+
+    return cls(
+        name=name,
+        completeness=completeness,
+        mass=mass,
+        position=position,
+        velocity=velocity,
+    )
+
+
+_LOADERS: Mapping[int, _PerturberDataLoader] = {
+    2: load_v2,
+    3: load_v3,
+}
+
+
--- tests/test_perturber_data.py
+++ tests/test_perturber_data.py
@@ -5,59 +5,59 @@ from pathlib import Path
 import numpy as np
 import pytest
 
-from novie_data.errors import InconsistentArrayLengthError, WrongArrayLengthError
 from novie_data.interface import NovieData
 from novie_data.perturber_data import PerturberData
+from novie_data_gen.builtins._verifier import InconsistentArrayLengthError, UnexpectedArrayLengthError
 
 
-def test_perturber_data_protocol() -> None:
+def test_protocol() -> None:
     """Test that the protocol is adhered to."""
     assert isinstance(PerturberData, NovieData)
 
 
-def test_perturber_data_init() -> None:
+def test_init() -> None:
     """Test the constructor."""
     num_frames = 20
-    pos = np.zeros((3, num_frames), dtype=np.float32)
-    vel = np.zeros((3, num_frames), dtype=np.float32)
-    mass = np.zeros(num_frames, dtype=np.float32)
+    pos = np.zeros((3, num_frames), dtype=np.float64)
+    vel = np.zeros((3, num_frames), dtype=np.float64)
+    mass = np.zeros(num_frames, dtype=np.float64)
     s = PerturberData(name="test", position=pos, velocity=vel, mass=mass)
     assert np.all(pos == s.position)
     assert np.all(vel == s.velocity)
     assert np.all(mass == s.mass)
 
 
-def test_perturber_data_init_wrong_position_dim() -> None:
+def test_init_wrong_position_dim() -> None:
     """Test the constructor with an invalid position array."""
     num_frames = 20
-    pos = np.zeros((2, num_frames), dtype=np.float32)
-    vel = np.zeros((3, num_frames), dtype=np.float32)
-    mass = np.zeros(num_frames, dtype=np.float32)
-    with pytest.raises(WrongArrayLengthError):
+    pos = np.zeros((2, num_frames), dtype=np.float64)
+    vel = np.zeros((2, num_frames), dtype=np.float64)
+    mass = np.zeros(num_frames, dtype=np.float64)
+    with pytest.raises(UnexpectedArrayLengthError):
         _ = PerturberData(name="test", position=pos, velocity=vel, mass=mass)
 
 
-def test_perturber_data_init_wrong_velocity_dim() -> None:
+def test_init_wrong_velocity_dim() -> None:
     """Test the constructor with an invalid velocity array."""
     num_frames = 20
-    pos = np.zeros((3, num_frames), dtype=np.float32)
-    vel = np.zeros((2, num_frames), dtype=np.float32)
-    mass = np.zeros(num_frames, dtype=np.float32)
-    with pytest.raises(WrongArrayLengthError):
-        _ = PerturberData(name="test", position=pos, velocity=vel, mass=mass)
-
-
-def test_perturber_data_init_inconsistent_dims() -> None:
-    """Test the constructor with an invalid velocity array."""
-    num_frames = 20
-    pos = np.zeros((3, num_frames), dtype=np.float32)
-    vel = np.zeros((3, num_frames + 1), dtype=np.float32)
-    mass = np.zeros(num_frames - 2, dtype=np.float32)
+    pos = np.zeros((3, num_frames), dtype=np.float64)
+    vel = np.zeros((2, num_frames), dtype=np.float64)
+    mass = np.zeros(num_frames, dtype=np.float64)
     with pytest.raises(InconsistentArrayLengthError):
         _ = PerturberData(name="test", position=pos, velocity=vel, mass=mass)
 
 
-def test_perturber_data_serde(tmp_path: Path) -> None:
+def test_init_inconsistent_dims() -> None:
+    """Test the constructor with an invalid velocity array."""
+    num_frames = 20
+    pos = np.zeros((3, num_frames), dtype=np.float64)
+    vel = np.zeros((3, num_frames + 1), dtype=np.float64)
+    mass = np.zeros(num_frames - 2, dtype=np.float64)
+    with pytest.raises(InconsistentArrayLengthError):
+        _ = PerturberData(name="test", position=pos, velocity=vel, mass=mass)
+
+
+def test_serde(tmp_path: Path) -> None:
     """Test serialization and deserialization.
 
     Parameters
@@ -68,19 +68,87 @@ def test_perturber_data_serde(tmp_path: Path) -> None:
     """
     output_path = tmp_path / "test.hdf5"
     num_frames = 20
-    pos = np.zeros((3, num_frames), dtype=np.float32)
-    vel = np.zeros((3, num_frames), dtype=np.float32)
-    mass = np.zeros(num_frames, dtype=np.float32)
+    pos = np.zeros((3, num_frames), dtype=np.float64)
+    vel = np.zeros((3, num_frames), dtype=np.float64)
+    mass = np.zeros(num_frames, dtype=np.float64)
     s = PerturberData(name="test", position=pos, velocity=vel, mass=mass)
     s.dump(output_path)
     t = PerturberData.load(output_path)
     assert s == t
 
 
-def test_perturber_data_deserialization_v2() -> None:
+def test_deserialization_v2() -> None:
     """Test deserialization of v2."""
     num_frames: int = 20
     input_path = Path("test_data/perturber_v2.hdf5")
     t = PerturberData.load(input_path)
 
     assert t.num_frames == num_frames
+
+
+def test_incremental_serde(tmp_path: Path) -> None:
+    """Test incremental serialization per frame.
+
+    Parameters
+    ----------
+    tmp_path : Path
+        The temporary directory to write to.
+
+    """
+    output_path = tmp_path / "test.hdf5"
+
+    s = PerturberData.empty(n=10)
+    s.dump(output_path)
+
+    s = PerturberData.load(output_path)
+    assert s.name == "UNKNOWN"
+    assert s.num_frames == 10
+    assert np.all(~s.completeness)
+
+    PerturberData.save_init(output_path, name="test")
+    s = PerturberData.load(output_path)
+    assert s.name == "test"
+    assert s.num_frames == 10
+    assert np.all(~s.completeness)
+
+    initial_position = np.array([0.01, 0.84, -12.43], dtype=np.float64).reshape(3)
+    initial_velocity = np.array([9.4, -0.04, 41], dtype=np.float64).reshape(3)
+    acceleration = np.array([0.01, -0.5, -0.0007], dtype=np.float64).reshape(3)
+    PerturberData.save_frame(
+        output_path,
+        0,
+        mass=421.46574,
+        position=initial_position,
+        velocity=initial_velocity,
+    )
+    s = PerturberData.load(output_path)
+    assert s.name == "test"
+    assert s.num_frames == 10
+    assert s.mass[0] == 421.46574
+    assert np.array_equal(s.position[:, 0], initial_position)
+    assert np.array_equal(s.velocity[:, 0], initial_velocity)
+
+    for i in range(1, 5):
+        current_time = 0.74 * i + 598
+        current_mass = current_time
+        current_velocity = acceleration * current_time + initial_velocity
+        current_position = 0.5 * acceleration * current_time**2 + initial_velocity * current_time + initial_position
+        PerturberData.save_frame(
+            output_path,
+            i,
+            mass=current_mass,
+            position=current_position.reshape(3),
+            velocity=current_velocity.reshape(3),
+        )
+    s = PerturberData.load(output_path)
+    assert s.name == "test"
+    assert s.num_frames == 10
+    for i in range(1, 5):
+        current_time = 0.74 * i + 598
+        current_mass = current_time
+        current_velocity = acceleration * current_time + initial_velocity
+        current_position = 0.5 * acceleration * current_time**2 + initial_velocity * current_time + initial_position
+        assert s.mass[i] == current_mass
+        assert np.array_equal(s.position[:, i], current_position)
+        assert np.array_equal(s.velocity[:, i], current_velocity)
+        assert s.completeness[i]

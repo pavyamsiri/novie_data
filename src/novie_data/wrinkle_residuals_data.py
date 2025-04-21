7783934ffcb1729f23647a29f7dc5bd2f690b888
Pavadol Yamsiri <pavyamsiri@gmail.com>
2025-04-21 21:31:38 +1000

    wrinkle_residuals: Use generated class

--- src/novie_data/wrinkle_residuals_data.py
+++ src/novie_data/wrinkle_residuals_data.py
@@ -1,58 +1,209 @@
-"""Data representing residuals residuals and errors between observed and simulated wrinkle data."""
-
 from __future__ import annotations
 
 import logging
-from typing import ClassVar
+from typing import TYPE_CHECKING, ClassVar, Protocol, Self, TypeAlias
 
+import numpy as np
+from h5py import File as Hdf5File
 from packaging.version import Version
+from typing_extensions import override
 
-from .signal_residuals_data import SignalResidualsData
+from novie_data_gen import (
+    check_axis_length,
+    get_dataset_from_hdf5,
+    get_file_version,
+    get_float_attr_from_hdf5,
+    get_int_attr_from_hdf5,
+    get_str_attr_from_hdf5,
+    read_dataset_from_hdf5_with_dtype,
+    verify_array_is_1d,
+    verify_array_is_2d,
+    verify_array_is_3d,
+    verify_array_is_4d,
+)
+
+if TYPE_CHECKING:
+    from collections.abc import Mapping
+    from pathlib import Path
+
+    from novie_data_gen import Array1D, Array2D, Array3D, Array4D
+
+    _Array1D_f32: TypeAlias = Array1D[np.float32]
+    _Array3D_f32: TypeAlias = Array3D[np.float32]
+    _Array2D_f32: TypeAlias = Array2D[np.float32]
+    _Array1D_f64: TypeAlias = Array1D[np.float64]
+    _Array3D_f64: TypeAlias = Array3D[np.float64]
+    _Array2D_f64: TypeAlias = Array2D[np.float64]
+
+LATEST_VERSION_V3 = Version("3.0.0")
+LATEST_VERSION_V4 = Version("4.0.0")
+
 
 log: logging.Logger = logging.getLogger(__name__)
 
 
-class WrinkleResidualsData(SignalResidualsData):
-    """Data class to store residuals and errors from wrinkle data processing.
+class _WrinkleResidualsDataLoader(Protocol):
+    def __call__(self, file: Hdf5File) -> WrinkleResidualsData: ...
 
-    Attributes
-    ----------
-    name : str
-        The name of the data set.
-    metric_name : str
-        The name of the metric used.
-    metric : Array3D[f32]
-        The value of the metric per point, frame and location.
-    summary : Array2D[f32]
-        The summary statistic of the metric per frame and location.
-    bin_values : Array1D[f32]
-        The central bin value for each bin.
 
-    """
-
+class WrinkleResidualsData:
     DATA_FILE_TYPE: ClassVar[str] = "WrinkleResiduals"
-    VERSION: ClassVar[Version] = Version("3.0.0")
+    VERSION: ClassVar[Version] = LATEST_VERSION_V4
+
+
+    def __init__(
+        self,
+        *,
+        bin_values: _Array1D_f64,
+        metric: _Array3D_f64,
+        summary: _Array2D_f64,
+        metric_name: str = "UNSET",
+        name: str = "UNKNOWN",
+    ) -> None:
+        num_bins = check_axis_length(
+            ((0, bin_values.shape), (0, metric.shape))
+        )
+        num_frames = check_axis_length(
+            ((1, metric.shape), (0, summary.shape))
+        )
+        num_locations = check_axis_length(
+            ((2, metric.shape), (1, summary.shape))
+        )
+        self.num_bins: int = num_bins
+        self.num_frames: int = num_frames
+        self.num_locations: int = num_locations
+        self.metric_name: str = metric_name
+        self.name: str = name
+        self.bin_values: _Array1D_f64 = bin_values
+        self.metric: _Array3D_f64 = metric
+        self.summary: _Array2D_f64 = summary
+
+    @override
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, type(self)):
+            return False
+        equality = True
+        equality &= self.num_bins == other.num_bins
+        equality &= self.num_frames == other.num_frames
+        equality &= self.num_locations == other.num_locations
+        equality &= self.metric_name == other.metric_name
+        equality &= self.name == other.name
+        equality &= np.array_equal(self.bin_values, other.bin_values)
+        equality &= np.array_equal(self.metric, other.metric)
+        equality &= np.array_equal(self.summary, other.summary)
+        return bool(equality)
 
     @classmethod
-    def get_data_file_type(cls) -> str:
-        """Return the data file type string for this class.
+    def empty(cls, *, num_bins: int, num_frames: int, num_locations: int) -> Self:
+        bin_values: _Array1D_f64 = np.zeros((num_bins,), dtype=np.float64)
+        metric: _Array3D_f64 = np.zeros((num_bins, num_frames, num_locations), dtype=np.float64)
+        summary: _Array2D_f64 = np.zeros((num_frames, num_locations), dtype=np.float64)
+        return cls(
+            bin_values=bin_values,
+            metric=metric,
+            summary=summary,
+        )
 
-        Returns
-        -------
-        file_type : str
-            The data file type.
+    @staticmethod
+    def load(path: Path) -> WrinkleResidualsData:
+        path = path.expanduser()
+        cls = WrinkleResidualsData
+        with Hdf5File(path, "r") as file:
+            assert str(file.attrs["type"]) == cls.DATA_FILE_TYPE
 
-        """
-        return cls.DATA_FILE_TYPE
+            file_version = get_file_version(file)
+            assert file_version.major in _LOADERS
+            data = _LOADERS[file_version.major](file)
+
+        log.info("Successfully dumped [cyan]%s[/cyan] to [magenta]%s[/magenta]", cls.__name__, path)
+        return data
 
     @classmethod
-    def get_version(cls) -> Version:
-        """Return the version for this class.
+    def migrate(cls, path: Path) -> None:
+        path = path.expanduser()
+        with Hdf5File(path, "r") as file:
+            assert str(file.attrs["type"]) == cls.DATA_FILE_TYPE
 
-        Returns
-        -------
-        version : Version
-            The file version.
+            file_version = get_file_version(file)
+            if file_version == LATEST_VERSION_V4:
+                return
+            file_version = get_file_version(file)
+            assert file_version.major in _LOADERS
+            data = _LOADERS[file_version.major](file)
 
-        """
-        return cls.VERSION
+        data.dump(path)
+
+    def dump(self, path: Path) -> None:
+        path = path.expanduser()
+        cls = type(self)
+        with Hdf5File(path, "w") as file:
+            file.attrs.create("type", str(cls.DATA_FILE_TYPE))
+            file.attrs.create("version", str(cls.VERSION))
+            file.attrs.create("metric_name", str(self.metric_name))
+            file.attrs.create("name", str(self.name))
+            _ = file.create_dataset("bin_values", data=self.bin_values)
+            _ = file.create_dataset("metric", data=self.metric)
+            _ = file.create_dataset("summary", data=self.summary)
+        log.info("Successfully dumped [cyan]%s[/cyan] to [magenta]%s[/magenta]", cls.__name__, path.absolute())
+
+
+
+
+def load_v3(file: Hdf5File) -> WrinkleResidualsData:
+    cls = WrinkleResidualsData
+    metric_name = get_str_attr_from_hdf5(file, "metric_name")
+    name = get_str_attr_from_hdf5(file, "name")
+    bin_values = verify_array_is_1d(read_dataset_from_hdf5_with_dtype(file, "bin_values", dtype=np.float32))
+    metric = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "metric", dtype=np.float32))
+    summary = verify_array_is_2d(read_dataset_from_hdf5_with_dtype(file, "summary", dtype=np.float32))
+    num_bins = check_axis_length(
+        ((0, bin_values.shape), (0, metric.shape),)
+    )
+    num_frames = check_axis_length(
+        ((1, metric.shape), (0, summary.shape),)
+    )
+    num_locations = check_axis_length(
+        ((2, metric.shape), (1, summary.shape),)
+    )
+
+    return cls(
+        metric_name=metric_name,
+        name=name,
+        bin_values=bin_values.astype(np.float64),
+        metric=metric.astype(np.float64),
+        summary=summary.astype(np.float64),
+    )
+
+
+def load_v4(file: Hdf5File) -> WrinkleResidualsData:
+    cls = WrinkleResidualsData
+    metric_name = get_str_attr_from_hdf5(file, "metric_name")
+    name = get_str_attr_from_hdf5(file, "name")
+    bin_values = verify_array_is_1d(read_dataset_from_hdf5_with_dtype(file, "bin_values", dtype=np.float64))
+    metric = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "metric", dtype=np.float64))
+    summary = verify_array_is_2d(read_dataset_from_hdf5_with_dtype(file, "summary", dtype=np.float64))
+    num_bins = check_axis_length(
+        ((0, bin_values.shape), (0, metric.shape),)
+    )
+    num_frames = check_axis_length(
+        ((1, metric.shape), (0, summary.shape),)
+    )
+    num_locations = check_axis_length(
+        ((2, metric.shape), (1, summary.shape),)
+    )
+
+    return cls(
+        metric_name=metric_name,
+        name=name,
+        bin_values=bin_values,
+        metric=metric,
+        summary=summary,
+    )
+
+
+_LOADERS: Mapping[int, _WrinkleResidualsDataLoader] = {
+    3: load_v3,
+    4: load_v4,
+}
+
+
--- tests/test_wrinkle_residuals_data.py
+++ tests/test_wrinkle_residuals_data.py
@@ -18,9 +18,9 @@ def test_wrinkle_residuals_data_init() -> None:
     num_radial_bins: int = 123
     num_frames: int = 7
     num_neighbourhoods: int = 87
-    angular_momentum = np.zeros(num_radial_bins, dtype=np.float32)
-    residuals = np.zeros((num_radial_bins, num_frames, num_neighbourhoods), dtype=np.float32)
-    sse = np.zeros((num_frames, num_neighbourhoods), dtype=np.float32)
+    angular_momentum = np.zeros(num_radial_bins, dtype=np.float64)
+    residuals = np.zeros((num_radial_bins, num_frames, num_neighbourhoods), dtype=np.float64)
+    sse = np.zeros((num_frames, num_neighbourhoods), dtype=np.float64)
     s = WrinkleResidualsData(
         name="test",
         metric_name="sse",
@@ -44,9 +44,9 @@ def test_wrinkle_residuals_data_serde(tmp_path: Path) -> None:
     num_radial_bins: int = 123
     num_frames: int = 7
     num_neighbourhoods: int = 87
-    angular_momentum = np.zeros(num_radial_bins, dtype=np.float32)
-    residuals = np.zeros((num_radial_bins, num_frames, num_neighbourhoods), dtype=np.float32)
-    sse = np.zeros((num_frames, num_neighbourhoods), dtype=np.float32)
+    angular_momentum = np.zeros(num_radial_bins, dtype=np.float64)
+    residuals = np.zeros((num_radial_bins, num_frames, num_neighbourhoods), dtype=np.float64)
+    sse = np.zeros((num_frames, num_neighbourhoods), dtype=np.float64)
     s = WrinkleResidualsData(
         name="test",
         metric_name="sse",

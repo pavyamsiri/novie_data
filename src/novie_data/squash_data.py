f7fe3efa901eb2553aea138b166e5b64af5c31ff
Pavadol Yamsiri <pavyamsiri@gmail.com>
2026-08-01 18:21:44 +1000

    squash_data: Add in std_z field
    want to test to see if areas of enhanced omegaz
    are due to increased std z

--- src/novie_data/squash_data.py
+++ src/novie_data/squash_data.py
@@ -25,6 +25,7 @@ if TYPE_CHECKING:
 
 
 LATEST_VERSION_V1 = Version("1.0.0")
+LATEST_VERSION_V2 = Version("2.0.0")
 
 
 log: logging.Logger = logging.getLogger(__name__)
@@ -36,7 +37,7 @@ class _SquashDataLoader(Protocol):
 
 class SquashData:
     DATA_FILE_TYPE: ClassVar[str] = "Squash"
-    VERSION: ClassVar[Version] = LATEST_VERSION_V1
+    VERSION: ClassVar[Version] = LATEST_VERSION_V2
 
 
     def __init__(
@@ -48,6 +49,7 @@ class SquashData:
         omegaphi_xy: Array3D[np.float64],
         omegar_xy: Array3D[np.float64],
         omegaz_xy: Array3D[np.float64],
+        stdz_xy: Array3D[np.float64],
         completeness: Array1D[np.bool_] | bool = True,
         extent: float = 25,
         max_jz: float = 6,
@@ -55,10 +57,10 @@ class SquashData:
         name: str = "UNKNOWN",
     ) -> None:
         num_bins = check_axis_length(
-            ((0, jphi_xy.shape), (1, jphi_xy.shape), (0, jr_xy.shape), (1, jr_xy.shape), (0, jz_xy.shape), (1, jz_xy.shape), (0, omegaphi_xy.shape), (1, omegaphi_xy.shape), (0, omegar_xy.shape), (1, omegar_xy.shape), (0, omegaz_xy.shape), (1, omegaz_xy.shape))
+            ((0, jphi_xy.shape), (1, jphi_xy.shape), (0, jr_xy.shape), (1, jr_xy.shape), (0, jz_xy.shape), (1, jz_xy.shape), (0, omegaphi_xy.shape), (1, omegaphi_xy.shape), (0, omegar_xy.shape), (1, omegar_xy.shape), (0, omegaz_xy.shape), (1, omegaz_xy.shape), (0, stdz_xy.shape), (1, stdz_xy.shape))
         )
         num_frames = check_axis_length(
-            ((2, jphi_xy.shape), (2, jr_xy.shape), (2, jz_xy.shape), (2, omegaphi_xy.shape), (2, omegar_xy.shape), (2, omegaz_xy.shape))
+            ((2, jphi_xy.shape), (2, jr_xy.shape), (2, jz_xy.shape), (2, omegaphi_xy.shape), (2, omegar_xy.shape), (2, omegaz_xy.shape), (2, stdz_xy.shape))
         )
         match completeness:
             case True:
@@ -81,6 +83,7 @@ class SquashData:
         self.omegaphi_xy: Array3D[np.float64] = omegaphi_xy
         self.omegar_xy: Array3D[np.float64] = omegar_xy
         self.omegaz_xy: Array3D[np.float64] = omegaz_xy
+        self.stdz_xy: Array3D[np.float64] = stdz_xy
 
     @override
     def __eq__(self, other: object) -> bool:
@@ -100,6 +103,7 @@ class SquashData:
         equality &= np.array_equal(self.omegaphi_xy, other.omegaphi_xy, equal_nan=True)
         equality &= np.array_equal(self.omegar_xy, other.omegar_xy, equal_nan=True)
         equality &= np.array_equal(self.omegaz_xy, other.omegaz_xy, equal_nan=True)
+        equality &= np.array_equal(self.stdz_xy, other.stdz_xy, equal_nan=True)
         return bool(equality)
 
     @classmethod
@@ -111,6 +115,7 @@ class SquashData:
         omegaphi_xy: Array3D[np.float64] = np.zeros((num_bins, num_bins, num_frames), dtype=np.float64)
         omegar_xy: Array3D[np.float64] = np.zeros((num_bins, num_bins, num_frames), dtype=np.float64)
         omegaz_xy: Array3D[np.float64] = np.zeros((num_bins, num_bins, num_frames), dtype=np.float64)
+        stdz_xy: Array3D[np.float64] = np.zeros((num_bins, num_bins, num_frames), dtype=np.float64)
         return cls(
             completeness=completeness,
             jphi_xy=jphi_xy,
@@ -119,6 +124,7 @@ class SquashData:
             omegaphi_xy=omegaphi_xy,
             omegar_xy=omegar_xy,
             omegaz_xy=omegaz_xy,
+            stdz_xy=stdz_xy,
         )
 
     @staticmethod
@@ -142,7 +148,7 @@ class SquashData:
             assert str(file.attrs["type"]) == cls.DATA_FILE_TYPE
 
             file_version = get_file_version(file)
-            if file_version == LATEST_VERSION_V1:
+            if file_version == LATEST_VERSION_V2:
                 return
             file_version = get_file_version(file)
             assert file_version.major in _LOADERS
@@ -167,6 +173,7 @@ class SquashData:
             _ = file.create_dataset("omegaphi_xy", data=self.omegaphi_xy)
             _ = file.create_dataset("omegar_xy", data=self.omegar_xy)
             _ = file.create_dataset("omegaz_xy", data=self.omegaz_xy)
+            _ = file.create_dataset("stdz_xy", data=self.stdz_xy)
         log.info("Successfully dumped [cyan]%s[/cyan] to [magenta]%s[/magenta]", cls.__name__, path.absolute())
 
     @classmethod
@@ -228,6 +235,7 @@ class SquashData:
         omegaphi_xy: Array2D[np.float64],
         omegar_xy: Array2D[np.float64],
         omegaz_xy: Array2D[np.float64],
+        stdz_xy: Array2D[np.float64],
     ) -> None:
         path = path.expanduser()
         if not path.is_file():
@@ -235,7 +243,7 @@ class SquashData:
             raise ValueError(msg)
 
         num_bins = check_axis_length(
-            ((0, jphi_xy.shape), (1, jphi_xy.shape), (0, jr_xy.shape), (1, jr_xy.shape), (0, jz_xy.shape), (1, jz_xy.shape), (0, omegaphi_xy.shape), (1, omegaphi_xy.shape), (0, omegar_xy.shape), (1, omegar_xy.shape), (0, omegaz_xy.shape), (1, omegaz_xy.shape))
+            ((0, jphi_xy.shape), (1, jphi_xy.shape), (0, jr_xy.shape), (1, jr_xy.shape), (0, jz_xy.shape), (1, jz_xy.shape), (0, omegaphi_xy.shape), (1, omegaphi_xy.shape), (0, omegar_xy.shape), (1, omegar_xy.shape), (0, omegaz_xy.shape), (1, omegaz_xy.shape), (0, stdz_xy.shape), (1, stdz_xy.shape))
         )
         cls.migrate(path)
         with Hdf5File(path, "a") as file:
@@ -259,6 +267,9 @@ class SquashData:
             )
             get_dataset_from_hdf5(file, "omegaz_xy").write_direct(
                 np.asarray(omegaz_xy, dtype=np.float64).reshape((num_bins, num_bins)), np.s_[:, :], np.s_[:, :, frame]
+            )
+            get_dataset_from_hdf5(file, "stdz_xy").write_direct(
+                np.asarray(stdz_xy, dtype=np.float64).reshape((num_bins, num_bins)), np.s_[:, :], np.s_[:, :, frame]
             )
         log.info("Successfully saved frame %d of [cyan]%s[/cyan] to [magenta]%s[/magenta]", frame, cls.__name__, path)
 
@@ -292,8 +303,40 @@ def load_v1(file: Hdf5File) -> SquashData:
     )
 
 
+def load_v2(file: Hdf5File) -> SquashData:
+    cls = SquashData
+    extent = get_float_attr_from_hdf5(file, "extent")
+    max_jz = get_float_attr_from_hdf5(file, "max_jz")
+    min_jz = get_float_attr_from_hdf5(file, "min_jz")
+    name = get_str_attr_from_hdf5(file, "name")
+    completeness = verify_array_is_1d(read_dataset_from_hdf5_with_dtype(file, "completeness", dtype=np.bool_))
+    jphi_xy = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "jphi_xy", dtype=np.float64))
+    jr_xy = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "jr_xy", dtype=np.float64))
+    jz_xy = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "jz_xy", dtype=np.float64))
+    omegaphi_xy = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "omegaphi_xy", dtype=np.float64))
+    omegar_xy = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "omegar_xy", dtype=np.float64))
+    omegaz_xy = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "omegaz_xy", dtype=np.float64))
+    stdz_xy = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "stdz_xy", dtype=np.float64))
+
+    return cls(
+        extent=extent,
+        max_jz=max_jz,
+        min_jz=min_jz,
+        name=name,
+        completeness=completeness,
+        jphi_xy=jphi_xy,
+        jr_xy=jr_xy,
+        jz_xy=jz_xy,
+        omegaphi_xy=omegaphi_xy,
+        omegar_xy=omegar_xy,
+        omegaz_xy=omegaz_xy,
+        stdz_xy=stdz_xy,
+    )
+
+
 _LOADERS: Mapping[int, _SquashDataLoader] = {
     1: load_v1,
+    2: load_v2,
 }
 
 

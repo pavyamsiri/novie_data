024f130aa4e5cd46aa0ca727c4f43d76e3acb95f
Pavadol Yamsiri <pavyamsiri@gmail.com>
2026-08-26 17:42:13 +1000

    Add completeness to HeightData

--- src/novie_data/height_data.py
+++ src/novie_data/height_data.py
@@ -7,10 +7,12 @@ import numpy as np
 from h5py import File as Hdf5File
 from novie_helpers import (
     check_axis_length,
+    get_dataset_from_hdf5,
     get_file_version,
     get_float_attr_from_hdf5,
     get_str_attr_from_hdf5,
     read_dataset_from_hdf5_with_dtype,
+    verify_array_is_1d,
     verify_array_is_3d,
 )
 from packaging.version import Version
@@ -19,7 +21,7 @@ if TYPE_CHECKING:
     from collections.abc import Mapping
     from pathlib import Path
 
-    from novie_helpers import Array3D
+    from novie_helpers import Array1D, Array2D, Array3D
 
 
 LATEST_VERSION_V1 = Version("1.0.0")
@@ -44,6 +46,7 @@ class HeightData:
         stdz_xy: Array3D[np.float32],
         vz_xy: Array3D[np.float32],
         z_xy: Array3D[np.float32],
+        completeness: Array1D[np.bool_] | bool = True,
         extent: float = 1,
         name: str = "UNKNOWN",
     ) -> None:
@@ -53,10 +56,19 @@ class HeightData:
         num_frames = check_axis_length(
             ((2, stdvz_xy.shape), (2, stdz_xy.shape), (2, vz_xy.shape), (2, z_xy.shape))
         )
+        match completeness:
+            case True:
+                completeness = np.ones((num_frames,), dtype=np.bool_)
+            case False:
+                completeness = np.zeros((num_frames,), dtype=np.bool_)
+            case _:
+                pass
+        assert completeness is not bool
         self.num_bins: int = num_bins
         self.num_frames: int = num_frames
         self.extent: float = extent
         self.name: str = name
+        self.completeness: Array1D[np.bool_] = completeness
         self.stdvz_xy: Array3D[np.float32] = stdvz_xy
         self.stdz_xy: Array3D[np.float32] = stdz_xy
         self.vz_xy: Array3D[np.float32] = vz_xy
@@ -71,6 +83,7 @@ class HeightData:
         equality &= self.num_frames == other.num_frames
         equality &= self.extent == other.extent
         equality &= self.name == other.name
+        equality &= np.array_equal(self.completeness, other.completeness, equal_nan=True)
         equality &= np.array_equal(self.stdvz_xy, other.stdvz_xy, equal_nan=True)
         equality &= np.array_equal(self.stdz_xy, other.stdz_xy, equal_nan=True)
         equality &= np.array_equal(self.vz_xy, other.vz_xy, equal_nan=True)
@@ -79,11 +92,13 @@ class HeightData:
 
     @classmethod
     def empty(cls, *, num_bins: int, num_frames: int) -> Self:
+        completeness: Array1D[np.bool_] = np.zeros((num_frames,), dtype=np.bool_)
         stdvz_xy: Array3D[np.float32] = np.zeros((num_bins, num_bins, num_frames), dtype=np.float32)
         stdz_xy: Array3D[np.float32] = np.zeros((num_bins, num_bins, num_frames), dtype=np.float32)
         vz_xy: Array3D[np.float32] = np.zeros((num_bins, num_bins, num_frames), dtype=np.float32)
         z_xy: Array3D[np.float32] = np.zeros((num_bins, num_bins, num_frames), dtype=np.float32)
         return cls(
+            completeness=completeness,
             stdvz_xy=stdvz_xy,
             stdz_xy=stdz_xy,
             vz_xy=vz_xy,
@@ -127,6 +142,7 @@ class HeightData:
             file.attrs.create("version", str(cls.VERSION))
             file.attrs.create("extent", self.extent, dtype=np.float64)
             file.attrs.create("name", str(self.name))
+            _ = file.create_dataset("completeness", data=self.completeness)
             _ = file.create_dataset("stdvz_xy", data=self.stdvz_xy)
             _ = file.create_dataset("stdz_xy", data=self.stdz_xy)
             _ = file.create_dataset("vz_xy", data=self.vz_xy)
@@ -147,13 +163,63 @@ class HeightData:
             is_compatible &= name == get_str_attr_from_hdf5(file, "name")
         return is_compatible
 
+    @classmethod
+    def save_init(cls, path: Path, *, extent: float, name: str) -> None:
+        path = path.expanduser()
+        if not path.is_file():
+            msg = f"Can't save initial data to {cls.__name__} as {path} doesn't exist!"
+            raise ValueError(msg)
 
+        cls.migrate(path)
+        with Hdf5File(path, "a") as file:
+            file.attrs.modify("extent", extent)
+            file.attrs.modify("name", name)
+        log.info("Successfully saved attributes of [cyan]%s[/cyan] to [magenta]%s[/magenta]", cls.__name__, path)
+
+    @classmethod
+    def save_frame(
+        cls,
+        path: Path,
+        frame: int,
+        *,
+        stdvz_xy: Array2D[np.float32],
+        stdz_xy: Array2D[np.float32],
+        vz_xy: Array2D[np.float32],
+        z_xy: Array2D[np.float32],
+    ) -> None:
+        path = path.expanduser()
+        if not path.is_file():
+            msg = f"Can't save initial data to {cls.__name__} as {path} doesn't exist!"
+            raise ValueError(msg)
+
+        num_bins = check_axis_length(
+            ((0, stdvz_xy.shape), (1, stdvz_xy.shape), (0, stdz_xy.shape), (1, stdz_xy.shape), (0, vz_xy.shape), (1, vz_xy.shape), (0, z_xy.shape), (1, z_xy.shape))
+        )
+        cls.migrate(path)
+        with Hdf5File(path, "a") as file:
+            get_dataset_from_hdf5(file, "completeness").write_direct(
+                np.asarray(True, dtype=np.bool_).reshape(1), np.s_[0], np.s_[frame]
+            )
+            get_dataset_from_hdf5(file, "stdvz_xy").write_direct(
+                np.asarray(stdvz_xy, dtype=np.float32).reshape((num_bins, num_bins)), np.s_[:, :], np.s_[:, :, frame]
+            )
+            get_dataset_from_hdf5(file, "stdz_xy").write_direct(
+                np.asarray(stdz_xy, dtype=np.float32).reshape((num_bins, num_bins)), np.s_[:, :], np.s_[:, :, frame]
+            )
+            get_dataset_from_hdf5(file, "vz_xy").write_direct(
+                np.asarray(vz_xy, dtype=np.float32).reshape((num_bins, num_bins)), np.s_[:, :], np.s_[:, :, frame]
+            )
+            get_dataset_from_hdf5(file, "z_xy").write_direct(
+                np.asarray(z_xy, dtype=np.float32).reshape((num_bins, num_bins)), np.s_[:, :], np.s_[:, :, frame]
+            )
+        log.info("Successfully saved frame %d of [cyan]%s[/cyan] to [magenta]%s[/magenta]", frame, cls.__name__, path)
 
 
 def load_v1(file: Hdf5File) -> HeightData:
     cls = HeightData
     extent = get_float_attr_from_hdf5(file, "extent")
     name = get_str_attr_from_hdf5(file, "name")
+    completeness = verify_array_is_1d(read_dataset_from_hdf5_with_dtype(file, "completeness", dtype=np.bool_))
     stdvz_xy = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "stdvz_xy", dtype=np.float32))
     stdz_xy = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "stdz_xy", dtype=np.float32))
     vz_xy = verify_array_is_3d(read_dataset_from_hdf5_with_dtype(file, "vz_xy", dtype=np.float32))
@@ -162,6 +228,7 @@ def load_v1(file: Hdf5File) -> HeightData:
     return cls(
         extent=extent,
         name=name,
+        completeness=completeness,
         stdvz_xy=stdvz_xy,
         stdz_xy=stdz_xy,
         vz_xy=vz_xy,
